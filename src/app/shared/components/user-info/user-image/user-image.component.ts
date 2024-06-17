f891bcaf52e6cd7a38e16eccd087e17824292b77
rocky_balboa <168601600+jsR1der@users.noreply.github.com>
2024-06-18 00:52:36 +0300

    feat: create basic component structure

---
+++ src/app/shared/components/user-info/user-image/user-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-image.component.html',
+  styleUrl: './user-image.component.scss'
+})
+export class UserImageComponent {
+
+}
---
+++ src/app/shared/components/user-info/user-name/user-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-name.component.html',
+  styleUrl: './user-name.component.scss'
+})
+export class UserNameComponent {
+
+}
