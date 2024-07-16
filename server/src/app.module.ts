3d7b32aa25b313cb9fed3d55e9e771cbe59c10ab
rocky_balboa <168601600+jsR1der@users.noreply.github.com>
2024-07-16 20:23:12 +0300

    dotenv issue

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
 
 @Module({
   imports: [],
+=======
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('PGHOST'),
+        port: configService.get<number>('DBPORT'),
+        password: configService.get<string>('PGPASSWORD'),
+        username: configService.get<string>('PGUSER'),
+        entities: [],
+        database: configService.get<string>('PGDATABASE'),
+        synchronize: configService.get<boolean>('synchronize'),
+        logging: configService.get<boolean>('logging'),
+        ssl: configService.get<boolean>('ssl'),
+      }),
+    }),
+  ],
+>>>>>>> 680c8cb (remove .env)
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+<<<<<<< HEAD
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+=======
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT'));
+}
+
+>>>>>>> 680c8cb (remove .env)
 bootstrap();
